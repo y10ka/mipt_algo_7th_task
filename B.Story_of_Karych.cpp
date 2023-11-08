6629d90be3addfac4c89008051732b07c9a716a5
y10ka <112867360+y10ka@users.noreply.github.com>
2023-11-08 14:41:14 +0300

    B update
    https://contest.yandex.ru/contest/51650/run-report/96383001/

--- B.Story_of_Karych.cpp
+++ B.Story_of_Karych.cpp
@@ -13,32 +13,32 @@ IntVector BuildPrefix(int64_t n) {
   return prefix_sum;
 }
 
-int64_t ArgMin(IntVector massive, int64_t border) {
+int64_t ArgMin(const IntVector& array, int64_t border) {
   auto min = inf;
   int64_t min_idx = 0;
   for (int64_t i = 0; i < border; ++i) {
-    if (massive[i] < min) {
-      min = massive[i];
+    if (array[i] < min) {
+      min = array[i];
       min_idx = i;
     }
   }
   return min_idx;
 }
 
-void FindMax(IntVector massive) {
-  auto size = static_cast<int64_t>(massive.size());
+void FindMax(const IntVector& array) {
+  auto size = static_cast<int64_t>(array.size());
   int64_t min = 0;
-  int64_t max = massive[1];
+  int64_t max = array[1];
   int64_t left = 0;
   int64_t cur_left = 0;
   int64_t right = 1;
   for (int64_t i = 1; i < size; ++i) {
-    if (massive[i - 1] < min) {
-      min = massive[i - 1];
+    if (array[i - 1] < min) {
+      min = array[i - 1];
       cur_left = i - 1;
     }
-    if (massive[i] - min > max) {
-      max = massive[i] - min;
+    if (array[i] - min > max) {
+      max = array[i] - min;
       right = i;
       left = cur_left;
     }
